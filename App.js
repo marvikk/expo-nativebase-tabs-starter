bccb630be0a21978b2682147e5f46ea925b3e1ac
Me <russzkij@yahoo.com>
2020-07-10 13:13:01 +0200

    custom header added

--- App.js
+++ App.js
@@ -2,6 +2,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import {
+  Header,
+  Left,
+  Body,
+  Title,
+  Button,
+  Icon,
+  Text
+} from 'native-base';
 
 import useCachedResources from './hooks/useCachedResources';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
@@ -10,8 +19,37 @@ import LinkingConfiguration from './navigation/LinkingConfiguration';
 import { StyleProvider } from 'native-base';
 import getTheme from './native-base-theme/components';
 import material from './native-base-theme/variables/material';
+import commonColor from './native-base-theme/variables/commonColor';
+
 
 const Stack = createStackNavigator();
+
+const CustomHeader = ({scene, previous, navigation}) => {
+  const {options} = scene.descriptor;
+  const title =
+    options.headerTitle !== undefined
+      ? options.headerTitle
+      : options.title !== undefined
+      ? options.title
+      : scene.route.name;
+
+  return (
+    <Header>
+      <Left>
+        {previous ? (
+          <Button transparent onPress={navigation.goBack}>
+            <Icon name="arrow-back" />
+          </Button>
+        ) : (
+          undefined
+        )}
+      </Left>
+      <Body>
+        <Title>{title}</Title>
+      </Body>
+    </Header>
+  );
+};
 
 export default function App(props) {
   const isLoadingComplete = useCachedResources();
@@ -20,12 +58,17 @@ export default function App(props) {
     return null;
   } else {
     return (
-    <StyleProvider style={getTheme(material)}>
+    <StyleProvider style={getTheme(commonColor)}>
       <View style={styles.container}>
         {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
         <NavigationContainer linking={LinkingConfiguration}>
           <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
+            <Stack.Screen name="Root" 
+            component={BottomTabNavigator}
+            options={{
+              header: (props) => <CustomHeader {...props} />,
+            }}
+             />
           </Stack.Navigator>
         </NavigationContainer>
       </View>
